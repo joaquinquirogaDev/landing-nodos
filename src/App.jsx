662a1dfa96e7f568b7ab30b565d25f30bc93ea6e
Joaquin Quiroga <joaquinquiroga22@gmail.com>
2023-08-18 12:19:57 -0300

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -11,3 +11,32 @@ function App() {
 }
 
 export default App
+
+//useRef 
+
+// import React, { useRef } from 'react';
+// import Header from './Header';
+
+// function LandingPage() {
+//   const componenteEspecificoRef = useRef(null);
+
+//   const scrollToComponent = () => {
+//     if (componenteEspecificoRef.current) {
+//       componenteEspecificoRef.current.scrollIntoView({ behavior: 'smooth' });
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <Header scrollToComponent={scrollToComponent} />
+
+//       {/* Otros componentes y contenido */}
+
+//       <div ref={componenteEspecificoRef} id="componente-especifico">
+//         {/* Contenido del componente específico */}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default LandingPage;
