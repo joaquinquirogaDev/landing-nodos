eca2bd938d24e3e810c8c901fb7fd088e5ec546f
linearediego <diegolineares@gmail.com>
2023-09-08 12:03:59 -0300

    footer

--- src/Components/Deporte/Deporte.jsx
+++ src/Components/Deporte/Deporte.jsx
@@ -2,7 +2,8 @@
 import style from './Deporte.module.css'
 //images
 import fondoDeporte from '../../assets/fondo/fondoDeporte.jpg'
-
+import deporte from '../../assets/deporte.png'
+import { Button } from '../Button/Button'
 export const Deporte = () => {
   return (
     <section className={style.boxDeporte}>
@@ -10,13 +11,20 @@ export const Deporte = () => {
             <img className={style.imgFondoDeporte} src={fondoDeporte} alt="deporte" />
 
             <div className={style.infoDeporte}>
-                <h1>Deportes San Agustín </h1>
-                <div className="">
-                    <p>Súmate a hacer deporte en nuestro cole</p>
-                    <p>Para chicas y chicos de 6 a 12 años </p>
-                    <p>de 18:15 hs a 19:45 hs - Todas las disciplinas </p>
+                <div className={style.textDeporte}>
+                    <h1>Deportes San Agustín </h1>
+                        <p>Súmate a hacer deporte en nuestro cole</p>
+                        <p>Para chicas y chicos de 6 a 12 años </p>
+                        <p>de 18:15 hs a 19:45 hs - Todas las disciplinas </p>
+                    <h1>COMIENZA LUNES 1 DE AGOSTO</h1>
+
+                    <Button>
+                        CONOCE MÁS
+                    </Button>
                 </div>
-                <h1>COMIENZA LUNES 1 DE AGOSTO</h1>
+                <div className={style.imgDeporte}>
+                    <img src={deporte} alt="" />
+                </div>
             </div>
         </div>
     </section>
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,23 +1,46 @@
+//data
 import { dropsRedes, Secciones } from "../../Data/data"
+//styles
 import style from './Footer.module.css'
+//images
+import logo from '../../assets/logoSanAgustin.png'
+import telefono from '../../assets/telefono.png'
+import youtube from '../../assets/youtube.png'
+import instagram from '../../assets/instagram.png'
+import facebook from '../../assets/facebook.png'
 export default function Footer() {
-    // for (const key in Secciones) {
-    //     console.log(key);
-    //     console.log(Secciones[key]);
-    // }
-    return <ul className={style.Container}>
-            {Object.keys(Secciones)?.map((item) => (
-                <div>
-                    <h1>{item}</h1>
-                    {Secciones[item].map((value) => (
-                        Object.entries(value).map(([item, value]) => (
-                            <div style={{width: '100%', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: '20px'}}>
-                                <h1>{item}: </h1>
-                                <p>{value}</p>
+    
+    return (
+        <>
+            <div className={style.Container}>
+                <div className={style.containerFooter}>
+                    <img className={style.containerFooterLogo} src={logo} alt="Colegio San Agustin" />
+                        {Object.keys(Secciones)?.map((item) => (
+                            <div>
+                                <h1 className={style.footerTitle}>{item}</h1>
+                                {Secciones[item].map((value) => (
+                                    Object.entries(value).map(([item, value]) => (
+                                        <div style={{width: '100%', display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '20px'}}>
+                                            <p>{item}: </p>
+                                            <p>{value}</p>
+                                        </div>
+                                    ))
+                                ))}
                             </div>
-                        ))
-                    ))}
+                        ))}
+
+                    </div>
+                    <div className={style.footerDown}>
+                        <h1 className={style.footerDownTitle}>@ 2024 Nodos Hub</h1>
+
+                        <div className={style.iconFooter}>
+                            <a href=""><img src={youtube} alt="youtube" /></a>
+                            <a href=""><img src={instagram} alt="instagram" /></a>
+                            <a href=""><img src={facebook} alt="facebook" /></a>
+                        </div>
+                    </div>
                 </div>
-            ))}
-        </ul>
+            </>
+    )
+   
 }
